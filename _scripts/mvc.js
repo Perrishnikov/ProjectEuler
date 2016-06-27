6e03cd9d7f330d02a39c99f7fca092a927c38677
Perrishnikov <perrishnikov@gmail.com>
2016-06-27 15:05:35 -0500

    first

--- _scripts/mvc.js
+++ _scripts/mvc.js
@@ -1,92 +1,61 @@
 "use strict";
 
-var view = {
-    problemViews : function(problem){ //pass in object
-        var problemNum = problem.problemNum;
-        var title = problem.title;
-        var text = problem.text;
-        var link = problem.link;
-        // var solution = problem.solution;
-        var customElements = problem.elements;
-        var script = problem.script;
-        var inputPrimaryName = problem.inputPrimaryName;
-        var inputSecondaryName = problem.inputSecondaryName || null;
-        var placeholderPrimary = problem.placeholderPrimary || "default";
-        var placeholderSecondary = problem.placeholderSecondary || "default";
 
-        addPanel();
-        addNav();
+// function Model() {
+//     // this._problemNum = problemNum;
+//     // console.log(problem);
+//     // this._title = title;
+// }
 
-        function addNav(){
-            $('#nav-start').append(" \
-            <li><a href='#'> #" + problemNum + " " + title + "</a></li> \
-            ");
-        };
+var Model = {
+    scriptsArray : [],
+    getScripts : function (dfd){
+        $.ajax({
+            url : "_scripts",
+            success: function (data) {
+                console.log("running ajax");
+                $(data).find("a").attr("href", function (index,val) {
 
-        function addPanel(){
-            $('#panel-start').append(" \
-            <div id='problem_" + problemNum + " ' class='panel panel-default problem'> \
-                <div class='panel-heading'> \
-                    <h3 class='panel-title'># " + problemNum + " <a href='" + link + "'>" + title + "</a></h3> \
-                </div> \
-                \
-                <div class='panel-body'> \
-                    <p>" + text + "</p> \
-                    <form class='form-inline'> \
-                        <div class='form-group'> \
-                            <label for='" + inputPrimaryName + "'>" + inputPrimaryName + "</label> \
-                            <input type='text' class='form-control' id='" + inputPrimaryName + "' placeholder='" + placeholderPrimary + "'> \
-                        </div> \
-                        <div class='form-group'> \
-                            <label for='" + inputSecondaryName + "'>" + inputSecondaryName + "</label> \
-                            <input class='form-control' id='" + inputSecondaryName + "' placeholder='" + placeholderSecondary + "'> \
-                        </div> \
-                        <button type='submit' class='btn btn-default'>Submit</button> \
-                        <button type='reset' class='btn btn-default'>Reset</button> \
-                    </form> \
-                </div> \
-            </div> \
-            ");
-        }
+                    if(val.match (/^problem_\d{2,3}.js$/)) { //loops one at a time. Looks for "problem_***.js"
+                    Model.scriptsArray.push(val);
+                    }
+                });
+            },
+            complete: function (){
+                // console.log(scriptsArray);
+                Model.scriptsArray.sort();
+                console.log(Model.scriptsArray);
+                Model.scriptsArray.forEach(function(element) { //loop through each item in array
+                    var path = "_scripts/" + element;
+                    $.getScript( path, function() {
+                        var valStripped = element.replace(/.js/, ''); //remove ".js"
+                        var _thisProblem = window[valStripped]; //find the variables in the DOM to use string as reference
+                        // return Model.scriptsArray;
+                        dfd.resolve(Model.scriptsArray);
+                    })
+                });
+            }
+        });
     }
 }
 
-function getScripts() {
-    var folder = "_scripts/";
-    var scriptsArray = [];
-    console.log("get scripts");
 
-    $.ajax({
-        url : folder,
-        success: function (data) {
-            $(data).find("a").attr("href", function (index,val) {
-
-                if(val.match (/^problem_\d{2,3}.js$/)) { //loops one at a time. Looks for "problem_***.js"
-                scriptsArray.push(val);
-                }
-            });
-        },
-        complete: function (){
-            console.log(scriptsArray);
-            scriptsArray.sort();
-            console.log(scriptsArray);
-            scriptsArray.forEach(function(element) { //loop through each item in array
-                var path = "_scripts/" + element;
-                $.getScript( path, function() {
-                    var valStripped = element.replace(/.js/, ''); //remove ".js"
-                    var _thisProblem = window[valStripped]; //find the variables in the DOM to use string as reference
-                    view.problemViews(_thisProblem);
-                })
-            });
-        }
-    });
-}
-
-$(function(){ //loads first
-    console.log("load");
-    getScripts();
-}());
-
-$(window).ready(function() { //loads afer READY
+$(document).ready(function() { //loads first
     console.log("ready");
+    var dfd = $.Deferred();
+    dfd.done(function( n ) {
+        console.log(n);
+        
+    });
+
+    Model.getScripts(dfd);
+    console.log(dfd);
+});
+
+
+
+
+
+$(window).load(function() { //loads afer READY
+    console.log("load");
 });
--- _scripts/problem_03.js
+++ _scripts/problem_03.js
@@ -5,8 +5,10 @@ var problem_03 = {
     text : "The prime factors of 13,195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600,851,475,143?",
     link : "https://projecteuler.net/problem=3",
     solution : "6857",
-    inputPrimaryName : "Value to solve for",
-    inputSecondaryName : null,
+    input01Name : "Number",
+    placeholder01 : 600851475143,
+    input02Name : null,
+    placeholder02 : null,
     elements : null,
 
     script : function(userVal01){
