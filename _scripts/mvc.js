5d7aea7f8974ef0472935ea8f0db734e649bc18c
Perrishnikov <perrishnikov@gmail.com>
2016-06-27 15:18:44 -0500

    edit

--- _scripts/mvc.js
+++ _scripts/mvc.js
@@ -1,5 +1,55 @@
 "use strict";
 
+var View = {
+    problemViews : function(problem){ //pass in object
+        var problemNum = problem.problemNum;
+        var title = problem.title;
+        var text = problem.text;
+        var link = problem.link;
+        // var solution = problem.solution;
+        var customElements = problem.elements;
+        var script = problem.script;
+        var inputPrimaryName = problem.inputPrimaryName;
+        var inputSecondaryName = problem.inputSecondaryName || null;
+        var placeholderPrimary = problem.placeholderPrimary || "default";
+        var placeholderSecondary = problem.placeholderSecondary || "default";
+
+        addPanel();
+        addNav();
+
+        function addNav(){
+            $('#nav-start').append(" \
+            <li><a href='#'> #" + problemNum + " " + title + "</a></li> \
+            ");
+        };
+
+        function addPanel(){
+            $('#panel-start').append(" \
+            <div id='problem_" + problemNum + " ' class='panel panel-default problem'> \
+                <div class='panel-heading'> \
+                    <h3 class='panel-title'># " + problemNum + " <a href='" + link + "'>" + title + "</a></h3> \
+                </div> \
+                \
+                <div class='panel-body'> \
+                    <p>" + text + "</p> \
+                    <form class='form-inline'> \
+                        <div class='form-group'> \
+                            <label for='" + inputPrimaryName + "'>" + inputPrimaryName + "</label> \
+                            <input type='text' class='form-control' id='" + inputPrimaryName + "' placeholder='" + placeholderPrimary + "'> \
+                        </div> \
+                        <div class='form-group'> \
+                            <label for='" + inputSecondaryName + "'>" + inputSecondaryName + "</label> \
+                            <input class='form-control' id='" + inputSecondaryName + "' placeholder='" + placeholderSecondary + "'> \
+                        </div> \
+                        <button type='submit' class='btn btn-default'>Submit</button> \
+                        <button type='reset' class='btn btn-default'>Reset</button> \
+                    </form> \
+                </div> \
+            </div> \
+            ");
+        }
+    }
+}
 
 // function Model() {
 //     // this._problemNum = problemNum;
@@ -44,12 +94,16 @@ $(document).ready(function() { //loads first
     console.log("ready");
     var dfd = $.Deferred();
     dfd.done(function( n ) {
+        var array = n;
         console.log(n);
-        
+        array.forEach(function(i){
+            View.problemViews(i);
+        });
+
     });
 
     Model.getScripts(dfd);
-    console.log(dfd);
+
 });
 
 
